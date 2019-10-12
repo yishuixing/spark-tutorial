2542e54af9376f6ed12207081edabbe6f20d04ee
anjunact <anjunact@qq.com>
2019-10-12 10:27:22 +0800

    'init'

---
+++ readme.md
@@ -0,0 +1,1 @@
+* 用maven 构建spark wordcount
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,15 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object WordCount {
+  def main(args: Array[String]) {
+    val inputFile = "F:\\scala\\git\\spark-tutorial\\src\\main\\resources\\word.txt"
+    val conf = new SparkConf().setAppName("WordCount").setMaster("local")
+    val sc = new SparkContext(conf)
+    val textFile = sc.textFile(inputFile)
+    val wordCount = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).
+      reduceByKey((a, b) => a + b)
+    wordCount.foreach(println)
+  }
+}
